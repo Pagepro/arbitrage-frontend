45c28ca3f8f1d1e8ed490ad6e3f43791e2944c0f
Tomasz Chmiel <tomasz.chmiel@pagepro.co>
2018-08-02 16:02:40 +0200

    some fixes

--- src/components/PairTable.tsx
+++ src/components/PairTable.tsx
@@ -6,66 +6,66 @@ interface IProps {
     pair: string
 }
 
-class PairTable extends React.Component <IProps, {}> {
+class PairTable extends React.Component<IProps, {}> {
 
-  constructor(props: any) {
-    super(props);
-  }
+    constructor(props: any) {
+        super(props);
+    }
 
-  public generateExchangePair(exchanges: string[]): object[] {
-    const exchangesPairs = [];
-    for (const firstExchange of exchanges) {
-        for (const secondExchange of exchanges) {
-            if (firstExchange !== secondExchange) {
-                exchangesPairs.push({buyExchange: firstExchange, sellExchange: secondExchange});
-                exchangesPairs.push({buyExchange: secondExchange, sellExchange: firstExchange});
+    public generateExchangePair(exchanges: string[]): object[] {
+        const exchangesPairs = [];
+        for (const firstExchange of exchanges) {
+            for (const secondExchange of exchanges) {
+                if (firstExchange !== secondExchange) {
+                    exchangesPairs.push({ buyExchange: firstExchange, sellExchange: secondExchange });
+                }
             }
         }
+        return exchangesPairs;
     }
-    return exchangesPairs;
-  }
 
-  public render() {
-    const {
-        exchanges
-    } = this.props
-    const exchangesPairs = this.generateExchangePair(exchanges);
-    const exchangesRows = exchangesPairs.map((item: any, index: any)=>{
-        const tableRowKey = `${this.props.pair}${item.buyExchange}${item.sellExchange}`;
-        return(
-            <TableRow 
-                pair={this.props.pair} 
-                buyExchange={item.buyExchange} 
-                sellExchange={item.sellExchange} 
-                key={tableRowKey} 
-            />
+    public render() {
+        const {
+            exchanges
+        } = this.props
+        const exchangesPairs = this.generateExchangePair(exchanges);
+        const exchangesRows = exchangesPairs.map((item: any, index: any) => {
+            const tableRowKey = `${this.props.pair}${item.buyExchange}${item.sellExchange}`;
+            return (
+                <TableRow
+                    pair={this.props.pair}
+                    buyExchange={item.buyExchange}
+                    sellExchange={item.sellExchange}
+                    key={tableRowKey}
+                    id={tableRowKey}
+                />
+            );
+        });
+        return (
+            <div>
+                <header>
+                    <h1>{this.props.pair}</h1>
+                </header>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th colSpan={2}>Exchanges</th>
+                            <th colSpan={3}>Prices</th>
+                        </tr>
+                        <tr>
+                            <th>Buy</th>
+                            <th>Sell</th>
+                            <th>Buy</th>
+                            <th>Sell</th>
+                            <th>Spread</th>
+                        </tr>
+                        {exchangesRows}
+                    </tbody>
+                </table>
+            </div>
         );
-    });
-    return (
-        <div>
-            <header>
-                <h1>{this.props.pair}</h1>
-            </header>
-            <table>
-                <tbody>
-                    <tr>
-                        <th colSpan={2}>Exchanges</th>
-                        <th colSpan={3}>Prices</th>
-                    </tr>
-                    <tr>
-                        <th>Buy</th>
-                        <th>Sell</th>
-                        <th>Buy</th>
-                        <th>Sell</th>
-                        <th>Spread</th>
-                    </tr>
-                    {exchangesRows}
-                </tbody>
-            </table>
-        </div>
-    );
-  }
-  
+    }
+
 }
 
 export default PairTable;
--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -5,6 +5,7 @@ import calculateSpread from '../utils/spreadCalculator';
 interface IProps {
     buyExchange: string,
     sellExchange: string,
+    id: string,
     pair: string
 }
 
@@ -15,114 +16,123 @@ interface IState {
     spreadValue: number
 }
 
-class TableRow extends React.Component <IProps, IState> {
+class TableRow extends React.Component<IProps, IState> {
 
     private ws: WebSocket;
     private timeout: any;
+    private isComponentMounted: boolean;
 
-  constructor(props: any) {
-    super(props);
+    constructor(props: any) {
+        super(props);
 
-    this.state = {
-        buyValue: 0,
-        justUpdated: false,
-        sellValue: 0,
-        spreadValue: 0
-    }
-    
-    this.clearState = this.clearState.bind(this);
-    this.updateSpread = this.updateSpread.bind(this);
-  }
-
-  public componentDidMount() {
-    this.createWebsocket();
-  }
-
-  public componentWillUnmount() {
-    this.ws.close();
-  }
-
-  public createWebsocket = () => {
-    this.ws = new WebSocket(websocketsConfig.exchanges);
-    this.ws.onmessage = (response) => {
-        clearTimeout(this.timeout);
-        this.timeout = setTimeout(this.clearState, 10000);
-
-        const {
-            ask: buyValue,
-            bid: sellValue,
-            pairName,
-            exchangeName
-        } = JSON.parse(response.data);
-
-        if (pairName === this.props.pair) {
-
-            if (exchangeName === this.props.buyExchange || exchangeName === this.props.sellExchange) {
-
-                this.setState(exchangeName === this.props.buyExchange ? 
-                    {
-                        buyValue,
-                        justUpdated: true,
-                        sellValue: this.state.sellValue,
-                        spreadValue: this.state.spreadValue
-                    } :
-                    {
-                        buyValue: this.state.buyValue,
-                        justUpdated: true,
-                        sellValue,
-                        spreadValue: this.state.spreadValue
-                    }, () => {
-                        this.updateSpread();
-                        setTimeout(() => {
-                            this.setState({
-                                justUpdated: false
-                            });
-                        }, 3000);
-                    }
-                );
-            }
+        this.state = {
+            buyValue: 0,
+            justUpdated: false,
+            sellValue: 0,
+            spreadValue: 0
         }
-    };
-    this.ws.onclose = () => {
+
+        this.clearState = this.clearState.bind(this);
+        this.updateSpread = this.updateSpread.bind(this);
+    }
+
+    public componentDidMount() {
+        this.isComponentMounted = true;
         this.createWebsocket();
-    };
-  }
+    }
 
-  public clearState() {
-    this.setState({
-        buyValue: 0,
-        justUpdated: false,
-        sellValue: 0,
-        spreadValue: 0
-    });
-  }
+    public componentWillUnmount() {
+        this.isComponentMounted = false;
+        this.ws.close();
+    }
 
-  public updateSpread() {
-      if (this.state.buyValue && this.state.sellValue) {
-        const spreadValue = calculateSpread(this.state.buyValue, this.state.sellValue);
-        this.setState({
+    public createWebsocket = () => {
+        this.ws = new WebSocket(websocketsConfig.exchanges);
+        this.ws.onmessage = (response) => {
+            clearTimeout(this.timeout);
+            this.timeout = setTimeout(this.clearState, 10000);
+
+            const {
+                ask: buyValue,
+                bid: sellValue,
+                pairName,
+                exchangeName
+            } = JSON.parse(response.data);
+
+            if (pairName === this.props.pair) {
+
+                if (exchangeName === this.props.buyExchange || exchangeName === this.props.sellExchange) {
+
+                    this.setState(exchangeName === this.props.buyExchange ?
+                        {
+                            buyValue,
+                            justUpdated: true,
+                            sellValue: this.state.sellValue,
+                            spreadValue: this.state.spreadValue
+                        } :
+                        {
+                            buyValue: this.state.buyValue,
+                            justUpdated: true,
+                            sellValue,
+                            spreadValue: this.state.spreadValue
+                        }, () => {
+                            this.updateSpread();
+                            setTimeout(() => {
+                                if (this.isComponentMounted) {
+                                    this.setState({
+                                        justUpdated: false
+                                    });
+                                }
+                            }, 3000);
+                        }
+                    );
+                }
+            }
+        };
+        this.ws.onclose = () => {
+            if (this.isComponentMounted) {
+                this.createWebsocket();
+            }
+        };
+    }
+
+    public clearState() {
+        if (this.isComponentMounted) {
+            this.setState({
+                buyValue: 0,
+                justUpdated: false,
+                sellValue: 0,
+                spreadValue: 0
+            });
+        }
+    }
+
+    public updateSpread() {
+        if (this.state.buyValue && this.state.sellValue) {
+            const spreadValue = calculateSpread(this.state.buyValue, this.state.sellValue);
+            this.setState({
+                spreadValue
+            });
+        }
+    }
+
+    public getSpreadValueCellClass() {
+        const {
             spreadValue
-        });
-      }
-  }
+        } = this.state
 
-  public getSpreadValueCellClass () {
-      const {
-        spreadValue
-      } = this.state
+        if (spreadValue === 0) {
+            return '';
+        }
 
-      if (spreadValue === 0) {
-        return '';
-      }
-      
-      return spreadValue > 0 ? 'positive' : 'negative';
-  }
+        return spreadValue > 0 ? 'positive' : 'negative';
+    }
 
-  public render() {
-      const buyExchangeLink = marketsConfig[this.props.buyExchange].marketLink(this.props.pair);
-      const sellExchangeLink = marketsConfig[this.props.sellExchange].marketLink(this.props.pair);
+    public render() {
+        const buyExchangeLink = marketsConfig[this.props.buyExchange].marketLink(this.props.pair);
+        const sellExchangeLink = marketsConfig[this.props.sellExchange].marketLink(this.props.pair);
         return (
-            <tr className={this.state.justUpdated ? 'updated' : ''}>
+            <tr className={this.state.justUpdated ? 'updated' : ''} id={this.props.id}>
                 <td>
                     <a target='_blank' href={buyExchangeLink}>
                         {this.props.buyExchange}
@@ -145,7 +155,7 @@ class TableRow extends React.Component <IProps, IState> {
             </tr>
         );
     }
-  
+
 }
 
 export default TableRow;
