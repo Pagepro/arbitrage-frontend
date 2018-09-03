ebb8799683d1c05b2aad045bf45c28bd958b5ca8
Patryk Komar <kontakt.patryk.komar@gmail.com>
2018-09-03 10:54:15 +0200

    InjectGlobal not needed anymore

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { injectGlobal } from 'styled-components';
 import App from './App';
 import './index.css';
-
-export const globalStyles = injectGlobal`
-  body {
-    overflow-x: hidden;
-  }
-`;
 
 ReactDOM.render(
   <App />,
