1b04b569f2d4b6ff8d055079d2f94ac0c379394c
Tomasz Chmiel <tomasz.chmiel@pagepro.co>
2018-09-03 13:40:27 +0200

    test

--- functions/exchangesWebsocketProxy.js
+++ functions/exchangesWebsocketProxy.js
@@ -1,36 +1,7 @@
-var faasGrip = require('faas-grip')
-
 exports.handler = function (event, _, callback) {
-  var ws
-  try {
-    ws = faasGrip.lambdaGetWebSocket(event)
-  } catch (err) {
-    callback(null, {
-      body: 'Not a WebSocket-over-HTTP request\n',
-      headers: {'Content-Type': 'text/plain'},
-      statusCode: 400
-    })
-    return
-  }
-
-  // if this is a new connection, accept it
-  if (ws.isOpening()) {
-    ws.accept()
-  }
-
-  // here we loop over any messages
-  while (ws.canRecv()) {
-    var message = ws.recv()
-
-    // if return value is null, then the connection is closed
-    if (message == null) {
-      ws.close()
-      break
-    }
-
-    // echo the message
-    ws.send(message)
-  }
-
-  callback(null, ws.toResponse())
+  callback(null, {
+    body: 'Not a WebSocket-over-HTTP request\n',
+    headers: {'Content-Type': 'text/plain'},
+    statusCode: 200
+  })
 }
