352e221c09dba0ae4b51542b10ea9d26e38faa07
Tomasz Chmiel <tomasz.chmiel@pagepro.co>
2018-09-03 13:59:00 +0200

    removed functions

--- functions/exchangesWebsocketProxy.js
+++
@@ -1,7 +0,0 @@
-exports.handler = function (event, _, callback) {
-  callback(null, {
-    body: 'Not a WebSocket-over-HTTP request\n',
-    headers: {'Content-Type': 'text/plain'},
-    statusCode: 200
-  })
-}
